dc696ec820087294aac3e3823cd01b6039722ba4
Light <social@kinmatik.com>
2023-09-05 17:10:14 +0300

    التحميل التلقائي

---
+++ Godot/Godot 4.0/Autoload/global.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var value = 0
---
+++ Godot/Godot 4.0/Autoload/second.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func _ready():
+	global.value += 10
+	$Value.text = str(global.value)
+	pass
+
+func _go_to():
+	get_tree().change_scene_to_file("res://first.tscn")
+	pass
